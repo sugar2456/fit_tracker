82691e3a992a8aa4698d26e50ae73b125ee8c30b
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-09-14 02:24:05 +0900

    cesium js関連の資材をコミット

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      os: false,
+    };
+    return config;
+  },
+  transpilePackages: ['cesium'],
 };
 
 export default nextConfig;
